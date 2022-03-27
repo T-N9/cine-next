6efa29ecb5041f7cbaad4af57f25e1597aff6b8f
T-N9 <tenyainmoelwin@gmail.com>
2022-03-27 12:08:49 +0630

    Test meta again

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux'
 import { makeLogoBig } from '../redux/navActiveSlice'
 import { HomeHero } from "../components"
 
-export default function Home({trending}) {
+export default function Home({ trending }) {
 
   const dispatch = useDispatch();
 
@@ -15,12 +15,40 @@ export default function Home({trending}) {
   return (
     <div>
       <Head>
-      <title>CINE | Live cinematic information</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="title" content="CINE | Live cinematic information" />
+        <meta name="description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them." />
+        <meta name="keywords"
+          content="cine, cinema, cinematic, information, movies, series, tv shows, tv, shows, torrents, tmdb, yts, download movies" />
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="author" content="TeNyain Moe Lwin" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <meta name="title" content="CINE | Live cinematic information" />
+        <meta name="description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them." />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cine-next.vercel.app/" />
+        <meta property="og:title" content="CINE | Live cinematic information" />
+        <meta property="og:description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them." />
+        <meta property="og:image" content="meta.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://cine-next.vercel.app/" />
+        <meta property="twitter:title" content="CINE | Live cinematic information" />
+        <meta property="twitter:description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them." />
+        <meta property="twitter:image" content="meta.png" />
       </Head>
-      
+
       <HomeHero
-        trending = {trending}
+        trending={trending}
       />
     </div>
   )
@@ -32,7 +60,7 @@ export async function getStaticProps(context) {
 
   return {
     props: {
-        trending : getTrending
-    }, 
+      trending: getTrending
+    },
   }
 }
